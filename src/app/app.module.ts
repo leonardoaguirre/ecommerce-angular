b941549bb610e5e97995acc99c1f5d0e5beb57e6
leonardoaguirre <62442754+leonardoaguirre@users.noreply.github.com>
2022-04-09 17:52:37 -0300

    create main component and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { BookstoreAppComponent } from './components/bookstore-app/bookstore-app.component';
+import { FiltersComponent } from './components/bookstore-app/filters/filters.component';
+import { ProductListComponent } from './components/bookstore-app/product-list/product-list.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { ProductItemComponent } from './components/bookstore-app/product-list/product-item/product-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    BookstoreAppComponent,
+    FiltersComponent,
+    ProductListComponent,
+    NavComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
